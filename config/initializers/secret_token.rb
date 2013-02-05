efa57298cc7758dae54d419cd4d62164da4950e8
Boska <mr.boska@gmail.com>
2013-02-06 00:23:13 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imdb::Application.config.secret_token = 'e773ab4a9f90718c00e4dfda3a2a589fe6b05373a9a4d32f8337f7e92d449794f57d6992b1aeae8b93c50d7ecb361a1f68985b4251203e465bef27873c4edb6c'

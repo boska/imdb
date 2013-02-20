6554f9cb3da5560518f7e687cd0c4ff6f98924fa
Boska <mr.boska@gmail.com>
2013-02-21 00:09:23 +0800

    ranking sort

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,7 @@ class MoviesController < ApplicationController
   # GET /movies
   # GET /movies.json
   def index
-    @movies = Movie.all
+    @movies = Movie.all.sort_by(&:average).reverse
 
     respond_to do |format|
       format.html # index.html.erb
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ActiveRecord::Base
   attr_accessible :director, :genre, :released, :star, :summary, :title, :writer
   letsrate_rateable 
+  scope :average, order("movies.average DESC")
 end
